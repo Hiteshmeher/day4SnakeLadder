3524ff5221cb943891e9c62156e0079e61211360
Hiteshmeher <meher.hitesh4@gmail.com>
2022-06-22 09:30:10 +0530

    Single player start with position 0

---
+++ src/day4SnakeLadder/day4SnakeLadder.java
@@ -0,0 +1,11 @@
+package day4SnakeLadder;
+
+public class day4SnakeLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Snake and Ladder");
+		int position = 0;
+		System.out.println("You are at 0");
+	}
+}

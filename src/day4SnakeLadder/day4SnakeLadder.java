c9960ba4512585b7c0e9190e90ed4e333132a344
Hiteshmeher <meher.hitesh4@gmail.com>
2022-06-22 18:43:41 +0530

    Checking the options

--- src/day4SnakeLadder/day4SnakeLadder.java
+++ src/day4SnakeLadder/day4SnakeLadder.java
@@ -8,5 +8,25 @@ public class day4SnakeLadder {
 	        System.out.println("Players starting Position is " +playerPosition);
 	        int number = 1 + (int)(Math.random() * 6);
 	        System.out.println("After rolling the dice i got " + number);
+	        
+	        int option = (int) Math.floor(Math.random()*3);
+	        if (option == 0) {
+	            System.out.println("No Play");
+	            playerPosition = playerPosition;
+	            System.out.println("Player current position is " + playerPosition);
+	        }
+	        else if (option == 1) {
+	            System.out.println("Ladder");
+	            playerPosition = playerPosition + number;
+	            System.out.println("Player current position is " +playerPosition);
+	        }
+	        else {
+	            System.out.println("Snake");
+	            playerPosition = playerPosition - number;
+	            if (playerPosition < 0) {
+	            	playerPosition = 0;
+	            }
+	            System.out.println("Player current position is " + playerPosition);
+	        }
 	    }
 	}

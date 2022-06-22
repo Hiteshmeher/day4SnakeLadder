a0b9f162030c72cdc594807e85510ff1967d8bf4
Hiteshmeher <meher.hitesh4@gmail.com>
2022-06-22 15:03:10 +0530

    Players position in game

--- src/day4SnakeLadder/day4SnakeLadder.java
+++ src/day4SnakeLadder/day4SnakeLadder.java
@@ -3,9 +3,9 @@ package day4SnakeLadder;
 public class day4SnakeLadder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Snake and Ladder");
-		int position = 0;
-		System.out.println("You are at 0");
+		 int playerPosition = 0;
+		 	System.out.println("Welcome to Snake Ladder Game");
+	        System.out.println("Players starting Position is " +playerPosition);
+	        System.out.println();
+	    }
 	}
-}
